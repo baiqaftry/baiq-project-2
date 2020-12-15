17d2cc5ce85f60b4ddc6e73a922cc863602e761e
baiqaftry <74760344+baiqaftry@users.noreply.github.com>
2020-12-16 03:27:39 +0700

    Add files via upload

---
+++ data/projectbaiq_1.js
@@ -0,0 +1,1 @@
+var json_projectbaiq_1 = {"type":"FeatureCollection","name":"projectbaiq_1","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1"},"geometry":{"type":"Point","coordinates":[107.734053707843017,-6.938398666842054]}}]}
